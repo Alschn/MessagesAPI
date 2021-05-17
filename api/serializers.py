6f4661c9ca09406e17edb66eee3d98931c495137
Alschn <adam.lisichin@gmail.com>
2021-05-17 22:18:24 +0200

    Add Message model and serializer
    Added simple Message model with content, views counter and datetime fields created_at, updated_at and its serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,1 +1,14 @@
 from rest_framework import serializers
+
+from api.models import Message
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+    views = serializers.IntegerField(required=False, read_only=True)
+    created_at = serializers.DateTimeField("%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+    updated_at = serializers.DateTimeField("%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['id', 'content', 'views', 'created_at', 'updated_at']
