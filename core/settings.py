7e9aefe7f3dcec9aa80530a8d3dac7881eda32b9
Alschn <adam.lisichin@gmail.com>
2021-05-17 22:23:34 +0200

    Install corsheaders
    Added corsheaders package and updated requirements.txt.

--- core/settings.py
+++ core/settings.py
@@ -138,3 +138,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# corsheaders
+CORS_ALLOW_ALL_ORIGINS = True
